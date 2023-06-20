d9e7afbcc7cd7f6643aa493a3cdff757f4edf937
VINICIUS ANDRE FERREIRA LUIZ <vinicius.afluiz@senacsp.edu.br>
2023-06-20 17:01:30 -0300

    Alterações e criação de controllers

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -7,18 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 use App\Models\{
-    User
+    TipoProduto
 };
-class Cargo extends Model
+
+class Produto extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $table  = 'cargos';
-    protected $primaryKey = 'id_cargo';
+    protected $table  = 'Produtos';
+    protected $primaryKey = 'id_produto';
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
     protected $fillabe = [
-        'cargo'
+        'id_tipo_produto',
+        'nome',
+        'descricao',
+        'foto',
+        'observacoes'
 
     ];
 
@@ -26,11 +31,11 @@ class Cargo extends Model
         RELACIONAMENTOS
      ------------------------------------------------------------------- */
 
-    public function usuarios(): object {
-        return $this->belongsTo(
-                                User::class,
-                                'id_cargo',
-                                'id_cargo');
+    public function tipo(): object {
+        return $this->hasOne(
+                                TipoProduto::class,
+                                'id_tipo_produto',
+                                'id_tipo_produto');
     }
 
 }
